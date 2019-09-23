574bcbdd4f20600570222c488b56e3912fd7e4cc
Roland Arsenault <roland@ccom.unh.edu>
2019-09-23 11:32:15 -0400

    Added buffer to allow delay (2 secs for now) between positions used for cmg calculations. This is to smooth out cmg in noisy position readings.

--- src/course_made_good_node.cpp
+++ src/course_made_good_node.cpp
@@ -2,15 +2,35 @@
 #include "marine_msgs/CourseMadeGoodStamped.h"
 #include "geographic_msgs/GeoPointStamped.h"
 #include "project11/gz4d_geo.h"
+#include <deque>
 
 ros::Publisher cmg_pub;
 
-gz4d::geo::Point<double,gz4d::geo::WGS84::LatLon> last_position;
+typedef gz4d::geo::Point<double,gz4d::geo::WGS84::LatLon> LatLonPoint;
+LatLonPoint last_position;
+
+typedef std::pair<ros::Time,LatLonPoint> TimePoint;
+std::deque<TimePoint> last_positions;
+
+/// Time in seconds between positions used to calculate course. Too close in time positions may result in noisy course.
+ros::Duration time_span(2.0);
 
 void positionCallback(const geographic_msgs::GeoPointStamped::ConstPtr& message)
 {
     gz4d::geo::Point<double,gz4d::geo::WGS84::LatLon> current_position(message->position.latitude,message->position.longitude,0.0);
-    if(last_position[0] != 9999)
+
+    while(!last_positions.empty() && (message->header.stamp - last_positions.front().first) > time_span)
+        last_positions.pop_front();
+    
+    if(!last_positions.empty())
+    {
+        auto azimuth_distance = gz4d::geo::WGS84::Ellipsoid::inverse(last_positions.front().second,current_position);
+        marine_msgs::CourseMadeGoodStamped cmgs;
+        cmgs.header = message->header;
+        cmgs.course = azimuth_distance.first;
+        cmg_pub.publish(cmgs);
+    }
+    else if(last_position[0] != 9999)
     {
         auto azimuth_distance = gz4d::geo::WGS84::Ellipsoid::inverse(last_position,current_position);
         marine_msgs::CourseMadeGoodStamped cmgs;
@@ -19,6 +39,7 @@ void positionCallback(const geographic_msgs::GeoPointStamped::ConstPtr& message)
         cmg_pub.publish(cmgs);
     }
     last_position = current_position;
+    last_positions.push_back(TimePoint(message->header.stamp,current_position));
 }
 
 int main(int argc, char **argv)
